881fb0ec74e37bcde5281e1a558d4843e87404b9
Yu Zou <zouy.fnst@fujitsu.com>
2024-07-01 17:32:24 +0800

    Workaround uint64 bug
    Signed-off-by: Yu Zou <zouy.fnst@fujitsu.com>

--- drivers/SmartThings/matter-switch/src/yeelight/init.lua
+++ drivers/SmartThings/matter-switch/src/yeelight/init.lua
@@ -15,7 +15,7 @@
 local capabilities = require "st.capabilities"
 local clusters = require "st.matter.clusters"
 local cluster_base = require "st.matter.cluster_base"
-local data_types = require "data_types"
+local data_types = require "yeelight/data_types"
 local device_lib = require "st.device"
 local utils = require "st.utils"
 local log = require "log"
